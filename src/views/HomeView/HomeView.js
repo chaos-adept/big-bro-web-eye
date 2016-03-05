1aeba2466f3ec506d18b00ba9d215bd14535ca9d
Denis Rykovanov <drykovanov@wiley.com>
2016-03-05 18:58:53 +0300

    [+] initial commit

---
+++ src/views/HomeView/HomeView.js
@@ -0,0 +1,15 @@
+/* @flow */
+import React from 'react'
+
+export class HomeView extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Bigbro Web Eye</h1>
+
+      </div>
+    )
+  }
+}
+
+export default HomeView
